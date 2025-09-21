35e2b344238fc1c0c17ad87065a92f219083c840
Addis <addis.ugr-4143-15@aau.edu.et>
2025-09-21 21:32:04 +0300

    test: remove cart page for debugging

--- src/app/(users-view)/(market)/shop/cart/page.tsx
+++ src/app/(users-view)/(market)/shop/cart/page.tsx
@@ -1,31 +1,5 @@
-import { Suspense } from "react";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import { ErrorBoundary } from "react-error-boundary";
-import { ErrorState } from "@/components/error-state";
-import LoadingState from "@/components/loading-state";
-import { auth } from "@/lib/auth";
-import { getQueryClient, trpc } from "@/trpc/server";
-
-const CartPage = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  if (!session) {
-    redirect("/sign-in");
-  }
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.cartItems.getMany.queryOptions());
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<LoadingState />}>
-        <ErrorBoundary fallback={<ErrorState />}>
-          <div>cart page</div>
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
-  );
-};
-
+const CartPage = () => {
+  return ( <div>cart page</div> );
+}
+ 
 export default CartPage;
