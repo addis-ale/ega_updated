7963b2efd04a82576e5dbe0f02362469c2572bf7
Addis <addis.ugr-4143-15@aau.edu.et>
2025-09-22 15:58:36 +0300

    feat: protect the admin view for only the authenticated user

--- src/app/(admins-view)/layout.tsx
+++ src/app/(admins-view)/layout.tsx
@@ -1,11 +1,20 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { DashboardSidebar } from "../modules/admin/ui/components/admin-dashboard-sidebar";
 import { DashboardNavbar } from "../modules/admin/ui/components/admin-dashboard-navbar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
 }
 const Layout = async ({ children }: Props) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/sign-in");
+  }
   //protect the admin route
   return (
     <SidebarProvider>
