05552657db155ee12ece13266cf278cfcbc0efa1
Addis <addis.ugr-4143-15@aau.edu.et>
2025-09-22 13:44:50 +0300

    fix(routing): delete route for debugging

--- src/app/(users-view)/layout.tsx
+++ src/app/(users-view)/layout.tsx
@@ -1,15 +1,7 @@
-import { Container } from "@/app/(users-view)/(market)/_components/container";
-import { Footer } from "@/app/(users-view)/(market)/_components/footer";
-import { Navbar } from "@/app/(users-view)/(market)/_components/navbar";
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1">
-        <Container>{children}</Container>
-      </main>
-      <Footer />
+      <main className="flex-1">{children}</main>
     </div>
   );
 };
