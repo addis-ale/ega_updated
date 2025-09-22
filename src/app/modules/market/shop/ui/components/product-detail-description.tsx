6113f785c9c9d3fe78214d3d5df0ac4d23958b7d
Addis <addis.ugr-4143-15@aau.edu.et>
2025-09-22 21:05:49 +0300

    Update product-detail-description.tsx

--- src/app/modules/market/shop/ui/components/product-detail-description.tsx
+++ src/app/modules/market/shop/ui/components/product-detail-description.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Calendar, Heart, Send, ShoppingCart } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn, formatPriceETB } from "@/lib/utils";
